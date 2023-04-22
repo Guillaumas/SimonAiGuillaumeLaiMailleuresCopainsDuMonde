0dbf9afb82f359273970031e120393ad905c09ba
Guillaumas <guillaume.gillet@epsi.fr>
2023-04-22 16:43:49 +0200

    Ajout WPF et methodes dans Tools pour convertir csv en DTO et DTO en DAL

---
+++ BICE_WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BICE.DTO;
+using BICE.SRV;
+using Microsoft.Win32;
+using System.IO;
+using BICE.WPF.Tools;
+using Microsoft.VisualBasic.FileIO;
+using BICE.DAL;
+
+namespace BICE_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadCsv_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog // Ouvre une fenêtre de dialogue pour choisir le fichier CSV à importer
+            {
+                Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*", // Filtre pour les fichiers CSV
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var materiels = CSVParser.ParseCsvFile(openFileDialog.FileName);
+
+                Materiel_depot_DAL materielDepot = new Materiel_depot_DAL(); // Crée un objet Materiel_depot_DAL
+
+                foreach (var materiel in materiels)
+                {
+                    Materiel_DAL materialDal = Converter.Materiel_DTO_To_DAL(materiel); // Convertit le DTO en DAL
+                    materielDepot.Insert(materialDal); // Insère le matériel dans la liste de matériel du dépôt
+                }
+            }
+
+            MessageBox.Show("Les données ont été importées avec succès.");
+
+        }
+    }
+}
---
+++ BICE_WPF/Tools/CSVParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using BICE.DTO;
+
+namespace BICE.WPF.Tools
+{
+    public class CSVParser
+    {
+        public static List<Material_DTO> ParseCsvFile(string csvFilePath)
+        {
+            List<Material_DTO> materiels = new List<Material_DTO>();
+
+            using (TextFieldParser parser = new TextFieldParser(csvFilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited; // Définit le type de fichier à traiter
+                parser.SetDelimiters(","); // Définit le délimiteur de champ
+
+                while (!parser.EndOfData) // Tant qu'il reste des lignes à lire
+                {
+                    
+                    string[] fields = parser.ReadFields(); // Lit la ligne
+
+                    Material_DTO materiel = new Material_DTO
+                    {
+                        Id = int.Parse(fields[0]),
+                        Denomination = fields[1],
+                        Code_barre = fields[2],
+                        Nombre_utilisations = int.Parse(fields[3]),
+                        Nombre_utilisations_limite = string.IsNullOrEmpty(fields[4]) ? (int?)null : int.Parse(fields[4]),
+                        Date_expiration = string.IsNullOrEmpty(fields[5]) ? (DateTime?)null : DateTime.Parse(fields[5]),
+                        Date_prochain_controle = string.IsNullOrEmpty(fields[6]) ? (DateTime?)null : DateTime.Parse(fields[6])
+                    }; // Crée un objet Materiel_DTO à partir des données de la ligne
+
+                    materiels.Add(materiel); // Ajoute l'objet à la liste
+                }
+            }
+
+            return materiels;
+        }
+    }
+}
