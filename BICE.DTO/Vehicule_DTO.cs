911fc1782e1e3626a0b41ded43021e33222cbd6f
Simiams <simon.convert@epsi.fr>
2023-04-26 21:01:02 +0200

    Fin de la gestion du materiel coté API (Update en retour d'intervention, update by stock.)

--- BICE.DTO/Vehicule_DTO.cs
+++ BICE.DTO/Vehicule_DTO.cs
@@ -2,6 +2,7 @@ namespace BICE.DTO;
 
 public class Vehicule_DTO : BICE_DTO
 {
+    public string Numero { get; set; }
     public string Immatriculation { get; set; }
     public bool Actif { get; set; }
 }
